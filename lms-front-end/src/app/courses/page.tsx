7da6a6368686f937342d444b3af53b15eed7e411
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2025-08-20 23:08:23 +0600

    update-ui

--- lms-front-end/src/app/courses/page.tsx
+++ lms-front-end/src/app/courses/page.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { getCourses } from "@/services/courseService";
-import AllCourseCardWrapper from "@/components/Course/AllCourseCardWrapper";
+import AllCourseCard from "@/components/Course/AllCourse";
 
 export default async function CourseDetailsPage() {
   try {
     const courses = await getCourses();
-    return <AllCourseCardWrapper courses={courses} />;
+
+    if (!courses || courses.length === 0) {
+      return (
+        <div className="flex items-center justify-center min-h-screen">
+          <p className="text-lg text-gray-600">
+            No courses available at the moment. Please try again later.
+          </p>
+        </div>
+      );
+    }
+
+    return <AllCourseCard courses={courses} />;
   } catch (error) {
     console.error("Error fetching courses:", error);
     return (
       <div className="flex items-center justify-center min-h-screen">
         <p className="text-lg text-gray-600">
-          No courses available at the moment. Please try again later.
+          Failed to load courses. Please try again later.
         </p>
       </div>
     );
