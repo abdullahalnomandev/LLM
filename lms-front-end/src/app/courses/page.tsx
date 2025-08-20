b96353e7b173e8d706e1ab696fb29aaae6d7dfff
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2025-08-21 00:19:49 +0600

    add

--- lms-front-end/src/app/courses/page.tsx
+++ lms-front-end/src/app/courses/page.tsx
@@ -3,28 +3,7 @@ import { getCourses } from "@/services/courseService";
 import AllCourseCard from "@/components/Course/AllCourse";
 
 export default async function CourseDetailsPage() {
-  try {
-    const courses = await getCourses();
+  const courses = await getCourses();
 
-    if (!courses || courses.length === 0) {
-      return (
-        <div className="flex items-center justify-center min-h-screen">
-          <p className="text-lg text-gray-600">
-            No courses available at the moment. Please try again later.
-          </p>
-        </div>
-      );
-    }
-
-    return <AllCourseCard courses={courses} />;
-  } catch (error) {
-    console.error("Error fetching courses:", error);
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <p className="text-lg text-gray-600">
-          Failed to load courses. Please try again later.
-        </p>
-      </div>
-    );
-  }
+  return <AllCourseCard courses={courses} />;
 }
